66b4aefaf9109faa15ddd6723ec591391f7353c9
Mackenzie Zastrow <3211021+zastrowm@users.noreply.github.com>
2025-07-01 10:01:32 -0400

    feat: Add test that fails due to #320 (#322)
    Co-authored-by: Mackenzie Zastrow <zastrowm@users.noreply.github.com>

--- tests-integ/test_model_openai.py
+++ tests-integ/test_model_openai.py
@@ -4,7 +4,11 @@ import pytest
 from pydantic import BaseModel
 
 import strands
-from strands import Agent
+from strands import Agent, tool
+
+if "OPENAI_API_KEY" not in os.environ:
+    pytest.skip(allow_module_level=True, reason="OPENAI_API_KEY environment variable missing")
+
 from strands.models.openai import OpenAIModel
 
 
@@ -36,10 +40,11 @@ def agent(model, tools):
     return Agent(model=model, tools=tools)
 
 
-@pytest.mark.skipif(
-    "OPENAI_API_KEY" not in os.environ,
-    reason="OPENAI_API_KEY environment variable missing",
-)
+@pytest.fixture
+def test_image_path(request):
+    return request.config.rootpath / "tests-integ" / "test_image.png"
+
+
 def test_agent(agent):
     result = agent("What is the time and weather in New York?")
     text = result.message["content"][0]["text"].lower()
@@ -47,10 +52,6 @@ def test_agent(agent):
     assert all(string in text for string in ["12:00", "sunny"])
 
 
-@pytest.mark.skipif(
-    "OPENAI_API_KEY" not in os.environ,
-    reason="OPENAI_API_KEY environment variable missing",
-)
 def test_structured_output(model):
     class Weather(BaseModel):
         """Extracts the time and weather from the user's message with the exact strings."""
@@ -64,3 +65,31 @@ def test_structured_output(model):
     assert isinstance(result, Weather)
     assert result.time == "12:00"
     assert result.weather == "sunny"
+
+
+@pytest.skip(
+    reason="OpenAI provider cannot use tools that return images - https://github.com/strands-agents/sdk-python/issues/320"
+)
+def test_tool_returning_images(model, test_image_path):
+    @tool
+    def tool_with_image_return():
+        with open(test_image_path, "rb") as image_file:
+            encoded_image = image_file.read()
+
+        return {
+            "status": "success",
+            "content": [
+                {
+                    "image": {
+                        "format": "png",
+                        "source": {"bytes": encoded_image},
+                    }
+                },
+            ],
+        }
+
+    agent = Agent(model=model, tools=[tool_with_image_return])
+    # NOTE - this currently fails with: "Invalid 'messages[3]'. Image URLs are only allowed for messages with role
+    # 'user', but this message with role 'tool' contains an image URL."
+    # See https://github.com/strands-agents/sdk-python/issues/320 for additional details
+    agent("Run the the tool and analyze the image")
